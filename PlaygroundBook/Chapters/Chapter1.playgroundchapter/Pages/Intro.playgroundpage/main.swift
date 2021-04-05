adfc785139fcb4386ac41d30a75fa0cd07b843e5
StephenFang <fangxuanmiao@gmail.com>
2021-04-05 23:56:21 +0800

    feat: 大致框架确定

---
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/Intro.playgroundpage/main.swift
@@ -0,0 +1,24 @@
+//#-hidden-code
+//
+//  See LICENSE folder for this template’s licensing information.
+//
+//  Abstract:
+//  The Swift file containing the source code edited by the user of this playground book.
+//
+//#-end-hidden-code
+
+//: # Introduction to Knowledge Graph
+//: Welcome to *A Tour In Knowledge Graph*, in this swift playground, you will learn the basic ideas about knowledge graph.
+//: - - -
+//: ## What is a Knowledge Graph?
+//: The knowledge graph represents a collection of interlinked descriptions of entities – real-world objects and events, situations or abstract concepts.
+//:
+//: ### Types of Recommendation Systems
+//: **Collaborative filtering** uses the known behavior of a group of users to make recommendations for others. For example, by using the data from other users who have similar preferences about a group of items, you can predict whether a particular user is likely to be interested in an item.
+//:
+//: **Content-based filtering** makes use of the comparison between items and the past preferences of a particular user. That is, the items which have similar properties to the ones that the user liked or checked previously are likely to be recommended.
+//:
+//: ## Item-to-Item Collaborative Filtering
+//: Item-based collaborative filtering was launched by Amazon.com in 1998, which dramatically improved the scalability of recommender systems. The algorithm calculates the similarities between different items in the dataset, then these similarity values are used to predict ratings for user-item pairs not present in the dataset.
+//: - - -
+//: ## [Next page](@next)
